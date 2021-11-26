4f0aaaca8548e535ee794b154002d8a35d9e51cc
colterize <85697865+colterize@users.noreply.github.com>
2021-11-26 10:18:11 +0700

    Initial Commit

---
+++ Fetch Data API/Fetch_Data_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Data_APIApp.swift
+//  Fetch Data API
+//
+//  Created by Yani . on 26/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Data_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
